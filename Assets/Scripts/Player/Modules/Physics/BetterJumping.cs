17d8213f3db7cb72f03ef06ed455049df98e2f9e
ChossyUK <chossymills@gmail.com>
2020-07-13 22:53:43 +0100

    The first modules
    Creating the 1st modules

---
+++ Assets/Scripts/Player/Modules/Physics/BetterJumping.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CedarWoodSoftware
+{
+	public class BetterJumping : MonoBehaviour
+	{
+        #region Variables
+        // Float value to increase the ridgidbody by
+        [SerializeField] float jumpFallMultiplier;
+
+        // Reference to the rigidbody
+        Rigidbody2D rb;
+        #endregion
+
+        #region Unity Base Methods
+        void Start()
+		{
+            // Get the rigidbody component
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+		void Update()
+		{
+            // If the rigidbody velocity is less than 0 apply the extra gravity 
+            if (rb.velocity.y < 0)
+            {
+                // Increase the gravity when falling to shorten the jump arc
+                rb.velocity += Vector2.up * Physics2D.gravity.y * jumpFallMultiplier * Time.deltaTime;
+            }
+        }
+		#endregion
+
+		#region User Methods
+			
+		#endregion
+	}
+}
---
+++ Assets/Scripts/Player/Modules/Physics/Collisions.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CedarWoodSoftware
+{
+	public class Collisions : MonoBehaviour
+	{
+        #region Variables
+        // Private fields
+        [Header("Collision Checks")]
+        // Ground checker radius
+        [SerializeField] float groundCheckerRadius;
+        // Wall checker radius
+        [SerializeField] float wallCheckerLength;
+        // Ledge checker radius
+        [SerializeField] float ledgeCheckerLength;
+        // Ground checker
+        [SerializeField] Transform groundChecker;
+        // Wall checker
+        [SerializeField] Transform wallChecker;
+        // Ledge checker
+        [SerializeField] Transform ledgeChecker;
+        // Ground layer
+        [SerializeField] LayerMask groundLayer;
+        // Wall layer
+        [SerializeField] LayerMask wallLayer;
+
+        // Public fields
+        public Transform WallChecker
+        {
+            get { return wallChecker; }
+        }
+
+        public float WallCheckerLength
+        {
+            get { return wallCheckerLength; }
+        }
+
+        public bool IsGrounded
+        {
+            get { return isGrounded; }
+            set { }
+        }
+
+        public bool IsTouchingWall
+        {
+            get { return isTouchingWall; }
+        }
+
+        public Transform LedgeChecker
+        {
+            get { return ledgeChecker; }
+        }
+
+        public float LedgeCheckerLength
+        {
+            get { return ledgeCheckerLength; }
+        }
+
+        public bool IsTouchingLedge
+        {
+            get { return isTouchingLedge; }
+        }
+
+        // Bools for physics
+        bool isGrounded;
+        bool isTouchingWall;
+        bool isTouchingLedge;
+        #endregion
+
+        #region Unity Base Methods
+        void FixedUpdate()
+        {
+            // Check the physics status
+            CheckPhysics();
+        }
+
+        void OnDrawGizmos()
+        {
+            // Draw gizmos to see the settings
+            Gizmos.DrawSphere(groundChecker.position, groundCheckerRadius);
+            Gizmos.DrawLine(wallChecker.position, new Vector3(wallChecker.position.x + wallCheckerLength, wallChecker.position.y, wallChecker.position.z));
+            Gizmos.DrawLine(ledgeChecker.position, new Vector3(ledgeChecker.position.x + ledgeCheckerLength, ledgeChecker.position.y, ledgeChecker.position.z));
+        }
+        #endregion
+
+        #region User Methods
+        void CheckPhysics()
+        {
+            // Check if the player is touching the ground
+            isGrounded = Physics2D.OverlapCircle(groundChecker.position, groundCheckerRadius, groundLayer);
+
+            // Check if the player is touching a wall
+            isTouchingWall = Physics2D.Raycast(wallChecker.position, transform.right, wallCheckerLength, wallLayer);
+
+            // Check if the player is touching a wall for ledge grab
+            isTouchingLedge = Physics2D.Raycast(ledgeChecker.position, transform.right, ledgeCheckerLength, wallLayer);
+        }
+        #endregion
+    }
+}
