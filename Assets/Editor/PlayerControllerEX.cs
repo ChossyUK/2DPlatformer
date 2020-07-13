a5ee415297aa5211d1702b84ebe42dbde62c475c
ChossyUK <chossymills@gmail.com>
2020-07-13 12:28:07 +0100

    Base Project

---
+++ Assets/Editor/PlayerControllerEX.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(PlayerController))]
+public class PlayerControllerEX : Editor
+{
+    PlayerController playerController;
+
+    private void OnEnable()
+    {
+        playerController = (PlayerController)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        GUILayout.Label("2D Platform Controller", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter, fontStyle = FontStyle.Bold, fontSize = 16 });
+        GUILayout.Label("Cedarwood Software Version: 1.0", new GUIStyle(GUI.skin.label) { alignment = TextAnchor.MiddleCenter });
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+        EditorGUILayout.Space();
+        base.DrawDefaultInspector();
+    }
+}
+#endif
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Variables
+
+    #endregion
+
+    #region Unity Base Methods
+    void Awake()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        
+    }
+
+    void OnDisable()
+    {
+        
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+    #endregion
+
+    #region User Methods
+
+    #endregion
+}
