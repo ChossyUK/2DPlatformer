20090f4d8a396a37a50aa71468681339ddd1cc0d
ChossyUK <chossymills@gmail.com>
2020-07-14 16:09:08 +0100

    Jumping & Flipping
    Finished of basic movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -18,16 +18,50 @@ public class PlayerController : MonoBehaviour, Controls.IPlayer1Actions
     // Jump multiplyer for variable jump height
     [SerializeField] float jumpMultiplier = 0.5f;
 
+    // Public getters
+    public float GravityMultiplier
+    {
+        get { return gravityMultiplier; }
+    }
 
+    public float Y
+    {
+        get { return y; }
+    }
 
+    public float X
+    {
+        get { return x; }
+    }
 
+    public float FacingDirection
+    {
+        get { return facingDirection; }
+    }
 
+    public bool FacingRight
+    {
+        get { return facingRight; }
+    }
 
+    public bool IsJumping
+    {
+        get { return isJumping; }
+    }
 
     // Floats for movement direction
     float x, y;
     float facingDirection = 1;
 
+    // Bools to control the player
+    public bool canMove = true;
+    [HideInInspector]
+    public bool canFlip = true;
+    [HideInInspector]
+    public bool canJump = true;
+    bool isJumping = false;
+    bool doubleJumped = false;
+    bool facingRight = true;
 
     // Reference to the collisions script
     Collisions coll;
@@ -72,7 +106,7 @@ public class PlayerController : MonoBehaviour, Controls.IPlayer1Actions
 
     void Update()
     {
-
+        CheckMovement();
     }
 
     void FixedUpdate()
@@ -83,6 +117,56 @@ public class PlayerController : MonoBehaviour, Controls.IPlayer1Actions
     #endregion
 
     #region User Methods
+    void CheckMovement()
+    {
+        // Check if we are touching the ground
+        if (coll.IsGrounded)
+        {
+            // Reset the is jumping bool
+            isJumping = false;
+
+            // Reset the is double jumping bool
+            doubleJumped = false;
+        }
+
+        // Flip the player according to direction
+        if (!facingRight && x > 0.1f || facingRight && x < -0.1f)
+        {
+            // call the flip method
+            Flip();
+        }
+    }
+
+
+    public void Flip()
+    {
+        // Check if flip is enabled
+        if (canFlip)
+        {
+            // Flip the facing direction
+            facingDirection *= -1;
+            // Flip between left and right
+            facingRight = !facingRight;
+            // Flip the player sprite 
+            transform.Rotate(0f, 180f, 0f);
+        }
+    }
+
+    public void Jump(Vector2 dir)
+    {
+        // Zero out the Y velocity
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+
+        // make the player jump
+        rb.velocity += dir * jumpForce;
+    }
+
+    void SetJumpBool()
+    {
+        // Set the jump bool to true
+        isJumping = true;
+    }
+
     public void OnMovement(InputAction.CallbackContext context)
     {
         // Get the input from the new control scheme
@@ -95,7 +179,34 @@ public class PlayerController : MonoBehaviour, Controls.IPlayer1Actions
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        
+        if(canJump)
+        {
+            // Check if button pressed
+            if (context.performed)
+            {
+                Invoke("SetJumpBool", 0.1f);
+
+                // Jump
+                if (coll.IsGrounded)
+                {
+                    Jump(Vector2.up);
+                }
+
+                // Double Jump
+                if (!coll.IsGrounded && !doubleJumped)
+                {
+                    Jump(Vector2.up);
+                    doubleJumped = true;
+                }
+            }
+
+            // Check if button released
+            if (context.canceled)
+            {
+                // Set the jump velocity depending on how long the button is held down
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jumpMultiplier);
+            }
+        }
     }
 
     public void OnDash(InputAction.CallbackContext context)
